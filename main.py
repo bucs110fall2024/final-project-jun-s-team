a4eb71bb86eaaeaa435a8912c56ec0b8b967734a
Jun Zheng <jzheng84@binghamton.edu>
2024-12-03 18:18:59 -0500

    11

--- main.py
+++ main.py
@@ -4,27 +4,6 @@ import pygame
 import pygame
 import random
 
-pygame.init()
-SCREEN = WIDTH, HEIGHT = 300, 500
-win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
-
-CELLSIZE = 20
-ROWS = (HEIGHT-120) // CELLSIZE
-COLS = WIDTH // CELLSIZE
-
-clock = pygame.time.Clock()
-FPS = 24
-
-BLACK = (21, 24, 29)
-BLUE = (31, 25, 76)
-RED = (252, 91, 122)
-WHITE = (255, 255, 255)
-
-img1 = pygame.image.load('assets/blue.png')
-img2 = pygame.image.load('assets/green.png')
-img3 = pygame.image.load('assets/pink.png')
-img4 = pygame.image.load('assets/red.png')
-
 
 
 def main():
