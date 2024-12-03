afe0042e78f104b8744017a6103b20f88f5e6299
Jun Zheng <jzheng84@binghamton.edu>
2024-12-03 18:39:45 -0500

    yy

--- src/blocks.py
+++ src/blocks.py
@@ -2,11 +2,6 @@ import random
 import pygame
 
 class Blocks:
-	# matrix
-	# 0   1   2   3
-	# 4   5   6   7
-	# 8   9   10  11
-	# 12  13  14  15
 
 	FIGURES = {
 		'I' : [[1, 5, 9, 13], [4, 5, 6, 7]],
