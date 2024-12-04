b65692acbd249c68411cf3b178002e103971a830
Jun Zheng <jzheng84@binghamton.edu>
2024-12-04 17:01:46 -0500

    11

--- src/controller.py
+++ src/controller.py
@@ -5,7 +5,25 @@ import random
 import pygame
 
 class Controller:
+
+ def mainloop(self):
+    #select state loop
+    
   
+  ### below are some sample loop states ###
+
+  def menuloop(self):
+    
+      #event loop
+
+      #update data
+
+      #redraw
+      
+  def gameloop(self):
+      #event loop
+
+      #update data
     
   def mainloop(self):
     #select state loop
