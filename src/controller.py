cfe6e94af46fe99414c35b21a7915f062cb69723
Jun Zheng <jzheng84@binghamton.edu>
2024-12-03 20:09:14 -0500

    ytt

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,4 @@
-from blocks import Blocks
+from blocks import *
 from colors import Colors
 from grid import grid
 import random 
@@ -8,10 +8,19 @@ import pygame
 
 class Controller:
   def __init__(self):
+	  self.grid = grid()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+	  self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+		self.game_over = False
+		self.score = 0
 		
-                
-  def __init__(self):
-    #setup pygame data
+  def get_random_block(self):
+		if len(self.blocks) == 0:
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		block = random.choice(self.blocks)
+		self.blocks.remove(block)
+		return block
     
   def mainloop(self):
     #select state loop
