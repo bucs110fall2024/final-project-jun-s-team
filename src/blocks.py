d31efcb85f1e525d6d4b14dc715490c33bc072f6
jzheng844 <jzheng84@binghamton.edu>
2024-12-05 00:20:56 -0500

    Update blocks.py

--- src/blocks.py
+++ src/blocks.py
@@ -1,43 +1,114 @@
-import random
 import pygame
-from colors import Colors
+import random
 
-pygame.init()
+COLUMNS = 10
+ROWS = 20
+CELL_SIZE = 40
+GAME_WIDTH, GAME_HEIGHT = COLUMNS * CELL_SIZE, ROWS * CELL_SIZE
 
-class Blocks:
-    # Load Assets
-    assets = {
-        1: pygame.image.load('assets/blue.png'),
-        2: pygame.image.load('assets/green.png'),
-        3: pygame.image.load('assets/red.png'),
-        4: pygame.image.load('assets/pink.png'),
-    }
+# side bar size 
+SIDEBAR_WIDTH = 200
+PREVIEW_HEIGHT_FRACTION = 0.7
+SCORE_HEIGHT_FRACTION = 1 - PREVIEW_HEIGHT_FRACTION
 
-    # Define Shapes
-    Shapes = {
-        'I': [[1, 5, 9, 13], [4, 5, 6, 7]],
-        'Z': [[4, 5, 9, 10], [2, 6, 5, 9]],
-        'S': [[6, 7, 9, 10], [1, 5, 6, 10]],
-        'L': [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
-        'J': [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
-        'T': [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
-        'O': [[1, 2, 5, 6]]
-    }
+# window
+PADDING = 20
+WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
+WINDOW_HEIGHT = GAME_HEIGHT + PADDING * 2
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.type = random.choice(list(Blocks.Shapes.keys()))
-        self.shape = Blocks.Shapes[self.type]
-        self.color = random.randint(1, 4)
-        self.orientation = 0
+# game behaviour 
+UPDATE_START_SPEED = 200
+MOVE_WAIT_TIME = 200
+ROTATE_WAIT_TIME = 200
+BLOCK_OFFSET = pygame.Vector2(COLUMNS // 2, -1)
 
-    def rotate(self):
-        self.rotation = (self.rotation + 1) % len(self.shape)
+# Colors 
+YELLOW = '#e7ff00'
+RED = '#fe003e'
+BLUE = '#3f00fe'
+GREEN = '#64ff00'
+PINK = '#fc49ab'
+CYAN = '#5fe8ff'
+ORANGE = '#ff7300'
+GRAY = '#1c1c1c'
+BLACK = '000000'
+LINE_COLOR = '#ffffff'
 
-    def current_positions(self):
-        return self.shape[self.rotation]
+# shapes
+TETROMINOS = {
+	'T': {'shape': [(0,0), (-1,0), (1,0), (0,-1)], 'color': PINK},
+	'O': {'shape': [(0,0), (0,-1), (1,0), (1,-1)], 'color': YELLOW},
+	'J': {'shape': [(0,0), (0,-1), (0,1), (-1,1)], 'color': BLUE},
+	'L': {'shape': [(0,0), (0,-1), (0,1), (1,1)], 'color': ORANGE},
+	'I': {'shape': [(0,0), (0,-1), (0,-2), (0,1)], 'color': CYAN},
+	'S': {'shape': [(0,0), (-1,0), (0,-1), (1,-1)], 'color': GREEN},
+	'Z': {'shape': [(0,0), (1,0), (0,-1), (-1,-1)], 'color': RED}
+}
 
-font = pygame.font.SysFont("verdana", 50)
-font2 = pygame.font.SysFont("verdana", 15)
+SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
 
+class Game: 
+    def __init__(self):
+        self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
+        self.display_surface = pygame.display.get_surface()
+        self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+        self.sprites = pygame.sprite.Group()
+    
+        self.line_surface = self.surface.copy()
+        self.line_surface.fill((0,255,0)) 
+        self.line_surface.set_colorkey((0,255,0))
+        self.line_surface.set_alpha(120)
+
+        self.tetromino = Tetromino('T', self.sprites)
+                
+    def move_down(self):
+
+
+    def draw_grid(self):
+        for col in range(1, COLUMNS):
+            x = col * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (x,0), (x,self.surface.get_height()), 1)
+        for row in range(1, ROWS):
+            y = row * CELL_SIZE
+            pygame.draw.line(self.line_surface, LINE_COLOR, (0,y), (self.surface.get_width(),y))
+            
+            self.surface.blit(self.line_surface, (0,0))
+    def input(self):
+        keys = pygame.key.get_pressed()
+        
+    def run(self):
+        self.surface.fill(GRAY)
+        self.sprites.draw(self.surface)
+        self.draw_grid()
+        self.display_surface.blit(self.surface, (PADDING,PADDING))
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+class Tetromino: 
+    def __init__(self, shape, group):
+
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+    def move_down(self):
+        for block in self.blocks:
+            block.pos.y += 1
+
+        
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE,CELL_SIZE))
+        self.image.fill(color)
+
+
+        #position
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
+    
+    def update(self):
+        self.rect.topleft = self.pos * CELL_SIZE
